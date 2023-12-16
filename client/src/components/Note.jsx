2e56a2a4858eca43b50c7217491fff1c0641801c
ΔΗΜΗΤΡΗΣ <63073176+1KRS@users.noreply.github.com>
2023-12-16 22:44:33 +0100

    Update Note.jsx

--- client/src/components/Note.jsx
+++ client/src/components/Note.jsx
@@ -1,8 +1,8 @@
-const Note = () => {
+const Note = (props) => {
   return (
     <div className="note">
-      <h1>Τίτλος</h1>
-      <p>Κείμενο</p>
+      <h1>{props.title}</h1>
+      <p>{props.content}</p>
     </div>
   );
 };
